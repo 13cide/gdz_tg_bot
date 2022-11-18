ee9771ab61ff4a0cbdca98414815f9bc06eeebd1
Justhack <nedikov.misha@yandex.ru>
2022-11-19 00:26:10 +0400

    12121

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import config
 import pickle
 
+from parsing import start_parsing
+from config import aleksey
 
 def solve():
     with open('Solved_tests.txt', 'wb') as f:
@@ -8,7 +10,14 @@ def solve():
 
 
 def main():
-    # some_parsing   TODO
+    #with open('Solved_tests.txt', 'rb') as f:
+     #   solved = pickle.load(f)
+
+    login = aleksey.login
+    password = aleksey.password
+
+    parsing = start_parsing.Parsing()
+    parsing.start_parsing(login, password)
     solve()
 
 
--- parsing/start_parsing.py
+++ parsing/start_parsing.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,7 +13,7 @@ from selenium.webdriver.common.alert import Alert
 from selenium.common.exceptions import NoSuchElementException
 
 from fake_useragent import UserAgent
-
+import time
 
 class Parsing:
     def __init__(self):
@@ -28,11 +30,28 @@ class Parsing:
         self.wait_start = WebDriverWait(self.driver, 7)
         self.wait_start_long = WebDriverWait(self.driver, 30)
         self.action = ActionChains(self.driver)
-    def start_parsing(self):
+    def start_parsing(self, login, password):
         self.driver.get(url='http://www.ymk.ru/')
+        self.driver.implicitly_wait(2)
+
+        login_area = self.driver.find_element(By.ID, 'login')
+        login_area.click()
+        login_area.clear()
+        login_area.send_keys(login)
+
+        password_area = self.driver.find_element(By.ID, 'psw')
+        password_area.click()
+        password_area.clear()
+        password_area.send_keys(password)
+
+        accept = self.driver.find_elements(By.CLASS_NAME, 'button')[0]
+        accept.click()
+
+        self.driver.get(url='http://www.ymk.ru/ymk/index.php?module=pupils&action=ShowDiary')
+        self.driver.implicitly_wait(2)
 
 
 
-if __name__ == '__main__':
-    parsing = Parsing()
-    parsing.start_parsing()
+        time.sleep(100)
+        self.driver.close()
+        self.driver.quit()
