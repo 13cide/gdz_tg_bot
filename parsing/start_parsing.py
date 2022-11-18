00db9486b1ada57a6e43acb2f90ac3f121fc31f2
Justhack <nedikov.misha@yandex.ru>
2022-11-19 01:23:53 +0400

    12121

--- parsing/start_parsing.py
+++ parsing/start_parsing.py
@@ -11,6 +11,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 
 from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+
 
 from fake_useragent import UserAgent
 import time
@@ -30,28 +32,47 @@ class Parsing:
         self.wait_start = WebDriverWait(self.driver, 7)
         self.wait_start_long = WebDriverWait(self.driver, 30)
         self.action = ActionChains(self.driver)
+
     def start_parsing(self, login, password):
-        self.driver.get(url='http://www.ymk.ru/')
-        self.driver.implicitly_wait(2)
+        try:
+            self.driver.get(url='http://www.ymk.ru/')
+            self.driver.implicitly_wait(2)
 
-        login_area = self.driver.find_element(By.ID, 'login')
-        login_area.click()
-        login_area.clear()
-        login_area.send_keys(login)
+            login_area = self.driver.find_element(By.ID, 'login')
+            login_area.click()
+            login_area.clear()
+            login_area.send_keys(login)
 
-        password_area = self.driver.find_element(By.ID, 'psw')
-        password_area.click()
-        password_area.clear()
-        password_area.send_keys(password)
+            password_area = self.driver.find_element(By.ID, 'psw')
+            password_area.click()
+            password_area.clear()
+            password_area.send_keys(password)
 
-        accept = self.driver.find_elements(By.CLASS_NAME, 'button')[0]
-        accept.click()
+            accept = self.driver.find_elements(By.CLASS_NAME, 'button')[0]
+            accept.click()
 
-        self.driver.get(url='http://www.ymk.ru/ymk/index.php?module=pupils&action=ShowDiary')
-        self.driver.implicitly_wait(2)
+            self.driver.get(url='http://www.ymk.ru/ymk/index.php?module=pupils&action=ShowDiary')
+            time.sleep(2)
 
+            soup = BeautifulSoup(self.driver.page_source, 'lxml')
 
-
-        time.sleep(100)
-        self.driver.close()
-        self.driver.quit()
+            marks = soup.find_all('a', {'class': 'diary_link'})
+            i = 0
+            for mark in marks:
+                print(mark.text)
+                if mark.text.strip().isdigit():
+                    try:
+                        marks_links = self.driver.find_elements(By.CLASS_NAME, 'diary_link')
+                        mark_link = marks_links[i]
+                        mark_link.click()
+                        time.sleep(2)
+                        self.driver.back()
+                    except:
+                        pass
+                i+=1
+        except NoSuchElementException as ex:
+            print(ex)
+        finally:
+            time.sleep(10000)
+            self.driver.close()
+            self.driver.quit()
